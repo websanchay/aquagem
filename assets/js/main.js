aa170685d2de1b4a4a3af33478d653d3db1956fd
websanchay <68428456+websanchay@users.noreply.github.com>
2021-10-16 23:36:20 +0530

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,6 +260,16 @@
 
 })()
 
-
 /**************************** delete cookies  *************************************** */
-$.cookie('my_cookie',null, {domain:'.thecreatives.in'})
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = www.thecreatives.in + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
+deleteAllCookies();
