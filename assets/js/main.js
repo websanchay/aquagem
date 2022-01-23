176217042bc30cc336bdfc2d16414e1e72e41610
websanchay <68428456+websanchay@users.noreply.github.com>
2022-01-23 20:20:48 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,253 +1,62 @@
-(function() {
-    "use strict";
-
-    /**
-     * Easy selector helper function
-     */
-    const select = (el, all = false) => {
-        el = el.trim()
-        if (all) {
-            return [...document.querySelectorAll(el)]
-        } else {
-            return document.querySelector(el)
-        }
-    }
-
-    /**
-     * Easy event listener function
-     */
-    const on = (type, el, listener, all = false) => {
-        let selectEl = select(el, all)
-        if (selectEl) {
-            if (all) {
-                selectEl.forEach(e => e.addEventListener(type, listener))
-            } else {
-                selectEl.addEventListener(type, listener)
-            }
-        }
-    }
-
-    /**
-     * Easy on scroll event listener 
-     */
-    const onscroll = (el, listener) => {
-        el.addEventListener('scroll', listener)
-    }
-
-    /**
-     * Navbar links active state on scroll
-     */
-    let navbarlinks = select('#navbar .scrollto', true)
-    const navbarlinksActive = () => {
-        let position = window.scrollY + 200
-        navbarlinks.forEach(navbarlink => {
-            if (!navbarlink.hash) return
-            let section = select(navbarlink.hash)
-            if (!section) return
-            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-                navbarlink.classList.add('active')
-            } else {
-                navbarlink.classList.remove('active')
-            }
-        })
-    }
-    window.addEventListener('load', navbarlinksActive)
-    onscroll(document, navbarlinksActive)
-
-    /**
-     * Scrolls to an element with header offset
-     */
-    const scrollto = (el) => {
-        let header = select('#header')
-        let offset = header.offsetHeight
-
-        if (!header.classList.contains('header-scrolled')) {
-            offset -= 16
-        }
-
-        let elementPos = select(el).offsetTop
-        window.scrollTo({
-            top: elementPos - offset,
-            behavior: 'smooth'
-        })
-    }
-
-    /**
-     * Header fixed top on scroll
-     */
-    let selectHeader = select('#header')
-    if (selectHeader) {
-        let headerOffset = selectHeader.offsetTop
-        let nextElement = selectHeader.nextElementSibling
-        const headerFixed = () => {
-            if ((headerOffset - window.scrollY) <= 0) {
-                selectHeader.classList.add('fixed-top')
-                nextElement.classList.add('scrolled-offset')
-            } else {
-                selectHeader.classList.remove('fixed-top')
-                nextElement.classList.remove('scrolled-offset')
-            }
-        }
-        window.addEventListener('load', headerFixed)
-        onscroll(document, headerFixed)
-    }
-
-    /**
-     * Back to top button
-     */
-    let backtotop = select('.back-to-top')
-    if (backtotop) {
-        const toggleBacktotop = () => {
-            if (window.scrollY > 100) {
-                backtotop.classList.add('active')
-            } else {
-                backtotop.classList.remove('active')
-            }
-        }
-        window.addEventListener('load', toggleBacktotop)
-        onscroll(document, toggleBacktotop)
-    }
-
-    /**
-     * Mobile nav toggle
-     */
-    on('click', '.mobile-nav-toggle', function(e) {
-        select('#navbar').classList.toggle('navbar-mobile')
-        this.classList.toggle('bi-list')
-        this.classList.toggle('bi-x')
-    })
-
-    /**
-     * Mobile nav dropdowns activate
-     */
-    on('click', '.navbar .dropdown > a', function(e) {
-        if (select('#navbar').classList.contains('navbar-mobile')) {
-            e.preventDefault()
-            this.nextElementSibling.classList.toggle('dropdown-active')
-        }
-    }, true)
-
-    /**
-     * Scrool with ofset on links with a class name .scrollto
-     */
-    on('click', '.scrollto', function(e) {
-        if (select(this.hash)) {
-            e.preventDefault()
-
-            let navbar = select('#navbar')
-            if (navbar.classList.contains('navbar-mobile')) {
-                navbar.classList.remove('navbar-mobile')
-                let navbarToggle = select('.mobile-nav-toggle')
-                navbarToggle.classList.toggle('bi-list')
-                navbarToggle.classList.toggle('bi-x')
-            }
-            scrollto(this.hash)
-        }
-    }, true)
-
-    /**
-     * Scroll with ofset on page load with hash links in the url
-     */
-    window.addEventListener('load', () => {
-        if (window.location.hash) {
-            if (select(window.location.hash)) {
-                scrollto(window.location.hash)
-            }
-        }
-    });
-
-    /**
-     * Hero carousel indicators
-     */
-    let heroCarouselIndicators = select("#hero-carousel-indicators")
-    let heroCarouselItems = select('#heroCarousel .carousel-item', true)
-
-    heroCarouselItems.forEach((item, index) => {
-        (index === 0) ?
-        heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
-            heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
-    });
-
-
-
-   /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item'
-      });
-
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        portfolioIsotope.on('arrangeComplete', function() {
-          AOS.refresh()
-        });
-      }, true);
-    }
-
-  });
-
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
-   * Animation on scroll
-   */
-  window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false
-    });
-  });
-
-})()
-
-/**************************** delete cookies  *************************************** */
 function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = www.thecreatives.in + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    for (var e = document.cookie.split(";"), t = 0; t < e.length; t++) {
+        var o = e[t],
+            s = o.indexOf("="); - 1 < s && o.substr(0, s);
+        document.cookie = www.thecreatives.in + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT"
     }
-}
+}! function() {
+    "use strict";
+    var i = (e, t) => (e = e.trim(), t ? [...document.querySelectorAll(e)] : document.querySelector(e)),
+        e = (t, e, o, s) => {
+            let a = i(e, s);
+            a && (s ? a.forEach(e => e.addEventListener(t, o)) : a.addEventListener(t, o))
+        },
+        t = (e, t) => { e.addEventListener("scroll", t) };
+    let s = i("#navbar .scrollto", !0);
+    var o = () => {
+        var o = window.scrollY + 200;
+        s.forEach(e => { var t;!e.hash || (t = i(e.hash)) && (o >= t.offsetTop && o <= t.offsetTop + t.offsetHeight ? e.classList.add("active") : e.classList.remove("active")) })
+    };
+    window.addEventListener("load", o), t(document, o);
+    var a = e => {
+        let t = i("#header"),
+            o = t.offsetHeight;
+        t.classList.contains("header-scrolled") || (o -= 16);
+        e = i(e).offsetTop;
+        window.scrollTo({ top: e - o, behavior: "smooth" })
+    };
+    
+    let l = i("#header");
+    if (l) {
+        var n = l.offsetTop;
+        let e = l.nextElementSibling;
+        var r = () => { n - window.scrollY <= 0 ? (l.classList.add("fixed-top"), e.classList.add("scrolled-offset")) : (l.classList.remove("fixed-top"), e.classList.remove("scrolled-offset")) };
+        window.addEventListener("load", r), t(document, r)
+    }
+    
+    let c = i(".back-to-top");
+    c && (r = () => { 100 < window.scrollY ? c.classList.add("active") : c.classList.remove("active") },
+     window.addEventListener("load", r), t(document, r)), e("click", ".mobile-nav-toggle", 
+     function(e) { i("#navbar").classList.toggle("navbar-mobile"), this.classList.toggle("bi-list"), 
+     this.classList.toggle("bi-x") }),
+    e("click", ".navbar .dropdown > a", function(e) { i("#navbar").classList.contains("navbar-mobile") && (e.preventDefault(), this.nextElementSibling.classList.toggle("dropdown-active")) }, !0), e("click", ".scrollto", function(e) {
+        if (i(this.hash)) {
+            e.preventDefault();
+            let t = i("#navbar");
+            if (t.classList.contains("navbar-mobile")) {
+                t.classList.remove("navbar-mobile");
+                let e = i(".mobile-nav-toggle");
+                e.classList.toggle("bi-list"), e.classList.toggle("bi-x")
+            }
+            a(this.hash)
+        }
+    }, !0), window.addEventListener("load", () => { window.location.hash && i(window.location.hash) && a(window.location.hash) });
+   
+    let d = i("#hero-carousel-indicators"),
+        v = i("#heroCarousel .carousel-item", !0);
+    v.forEach((e, t) => { d.innerHTML += 0 === t ? "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + t + "' class='active'></li>" : "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + t + "'></li>" }), window.addEventListener("load", () => { AOS.init({ duration: 1e3, easing: "ease-in-out", once: !0, mirror: !1 }) })
+}(), deleteAllCookies();
 
-deleteAllCookies();
+ 
+
+
